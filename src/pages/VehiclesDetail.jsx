0c483d78211713aab5bff616a58ef651a8b25dfc
alviyudha <alviyudha@gmail.com>
2024-11-11 16:24:41 +0700

    test protect branch 2

--- src/pages/VehiclesDetail.jsx
+++ src/pages/VehiclesDetail.jsx
@@ -1,19 +1,25 @@
 // VehiclesDetail.js
-import React, { Suspense } from 'react';
-import NavbarBootstrap from '../components/NavbarBootstrap';
-import Footer from '../components/Footer';
-import LoadingComp from '../components/LoadingComp';
-import ReactFloatingWspComp from '../components/ReactFloatingWspComp';
-const VehiclesDetComp = React.lazy(() => import('../components/VehiclesDetComp'));
-const AccordionSpek = React.lazy(() => import('../components/AccordionSpek'));
-const InfoLengkapComp = React.lazy(() => import('../components/InfoLengkapComp'));
-const KatalogImageComp = React.lazy(() => import('../components/KatalogImageComp'));
+import React, { Suspense } from "react";
+import NavbarBootstrap from "../components/NavbarBootstrap";
+import Footer from "../components/Footer";
+import LoadingComp from "../components/LoadingComp";
+import ReactFloatingWspComp from "../components/ReactFloatingWspComp";
+const VehiclesDetComp = React.lazy(() =>
+  import("../components/VehiclesDetComp")
+);
+const AccordionSpek = React.lazy(() => import("../components/AccordionSpek"));
+const InfoLengkapComp = React.lazy(() =>
+  import("../components/InfoLengkapComp")
+);
+const KatalogImageComp = React.lazy(() =>
+  import("../components/KatalogImageComp")
+);
 
 export default function VehiclesDetail() {
   return (
     <>
       <NavbarBootstrap />
-      <Suspense fallback={ <LoadingComp/>}>
+      <Suspense fallback={<LoadingComp />}>
         <KatalogImageComp />
         <VehiclesDetComp />
         <div className="container shadow pt-2 mt-5 mb-5">
